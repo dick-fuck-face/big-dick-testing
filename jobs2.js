45b1875b47662898c3b8702807213f4ac84a1bf7
dick-fuck-face <124760072+dick-fuck-face@users.noreply.github.com>
2023-02-08 13:20:51 -0800

    Update jobs2.js

--- jobs2.js
+++ jobs2.js
@@ -1,66 +1,36 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <style>
-      .emoji {
-        position: absolute;
-        width: 50px;
-        height: 50px;
-        font-size: 2em;
-        user-select: none;
-        cursor: grab;
-      }
-    </style>
-  </head>
-  <body>
-    <div class="emoji">😀</div>
+const emoji = document.querySelector('.emoji');
+let isDragging = false;
+let currentX;
+let currentY;
+let initialX;
+let initialY;
+let xOffset = 0;
+let yOffset = 0;
 
-    <audio id="sound" src="sound.mp3"></audio>
+emoji.addEventListener("mousedown", dragStart);
+emoji.addEventListener("mouseup", dragEnd);
+emoji.addEventListener("mouseout", dragEnd);
+emoji.addEventListener("mousemove", drag);
 
-    <script>
-      const emoji = document.querySelector('.emoji');
-      let isDragging = false;
-      let currentX;
-      let currentY;
-      let initialX;
-      let initialY;
-      let xOffset = 0;
-      let yOffset = 0;
+function dragStart(e) {
+  initialX = e.clientX;
+  initialY = e.clientY;
+  xOffset = currentX - initialX;
+  yOffset = currentY - initialY;
+  isDragging = true;
+}
 
-      emoji.addEventListener("mousedown", dragStart);
-      emoji.addEventListener("mouseup", dragEnd);
-      emoji.addEventListener("mouseout", dragEnd);
-      emoji.addEventListener("mousemove", drag);
-      emoji.addEventListener("click", playSound);
+function dragEnd(e) {
+  isDragging = false;
+}
 
-      function dragStart(e) {
-        initialX = e.clientX;
-        initialY = e.clientY;
-        xOffset = currentX - initialX;
-        yOffset = currentY - initialY;
-        isDragging = true;
-      }
+function drag(e) {
+  if (isDragging) {
+    e.preventDefault();
+    currentX = e.clientX + xOffset;
+    currentY = e.clientY + yOffset;
 
-      function dragEnd(e) {
-        isDragging = false;
-      }
-
-      function drag(e) {
-        if (isDragging) {
-          e.preventDefault();
-          currentX = e.clientX + xOffset;
-          currentY = e.clientY + yOffset;
-
-          emoji.style.top = currentY + "px";
-          emoji.style.left = currentX + "px";
-        }
-      }
-
-      function playSound() {
-        const sound = document.getElementById("sound");
-        sound.currentTime = 0;
-        sound.play();
-      }
-    </script>
-  </body>
-</html>
+    emoji.style.top = currentY + "px";
+    emoji.style.left = currentX + "px";
+  }
+}
