7ef44b29c38df49c913c78fd7a85ea255c5ff620
dick-fuck-face <124760072+dick-fuck-face@users.noreply.github.com>
2023-02-08 14:21:17 -0800

    Delete jobs.js

--- jobs.js
+++
@@ -1,9 +0,0 @@
-const emoji = document.querySelector('.emoji');
-
-emoji.addEventListener('mouseenter', () => {
-  emoji.style.transform = 'rotate(360deg)';
-});
-
-emoji.addEventListener('mouseleave', () => {
-  emoji.style.transform = 'rotate(0)';
-});
