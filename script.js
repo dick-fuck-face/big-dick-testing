b7e90d092cf6b48cd69f6c7381c386e472696d68
dick-fuck-face <124760072+dick-fuck-face@users.noreply.github.com>
2023-02-09 18:54:37 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
 document.addEventListener("DOMContentLoaded", function() {
+  const box = document.querySelector("#box");
+  const audioFolder = "https://api.github.com/repos/dick-fuck-face/big-dick-testing/contents/audio";
+  let audioFiles = [];
+  let currentAudio;
 
-const boxes = document.querySelectorAll("#box, #box2");
-const audioFiles = [
-  "https://github.com/dick-fuck-face/big-dick-testing/blob/main/sound.mp3",
-  "https://github.com/dick-fuck-face/big-dick-testing/blob/main/sound.mp3",
-  "https://github.com/dick-fuck-face/big-dick-testing/blob/main/sound.mp3"
-];
+  fetch(audioFolder)
+    .then(res => res.json())
+    .then(data => {
+      audioFiles = data.map(file => file.download_url);
+    });
 
-boxes.forEach(box => {
-  const randomIndex = Math.floor(Math.random() * audioFiles.length);
-  const randomAudioFile = audioFiles[randomIndex];
-  const audio = new Audio(randomAudioFile);
+  box.addEventListener("click", function() {
+    if (audioFiles.length) {
+      const randomAudio = audioFiles[Math.floor(Math.random() * audioFiles.length)];
+      if (currentAudio) {
+        currentAudio.pause();
+        currentAudio.currentTime = 0;
+      }
+      currentAudio = new Audio(randomAudio);
+      currentAudio.play();
+    }
+  });
 
   box.addEventListener("mouseover", function() {
     box.style.transform = "scale(1.2)";
