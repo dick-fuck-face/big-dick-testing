7854c616075061ce02d9abdd29974bbd4331839e
dick-fuck-face <124760072+dick-fuck-face@users.noreply.github.com>
2023-02-09 16:01:22 -0800

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
   const box = document.querySelector("#box");
   const audioFolder = "https://api.github.com/repos/dick-fuck-face/big-dick-testing/contents/audio";
   let audioFiles = [];
+  let currentAudio;
 
   fetch(audioFolder)
     .then(res => res.json())
@@ -12,55 +13,56 @@ document.addEventListener("DOMContentLoaded", function() {
   box.addEventListener("click", function() {
     if (audioFiles.length) {
       const randomAudio = audioFiles[Math.floor(Math.random() * audioFiles.length)];
-      const audio = new Audio(randomAudio);
-      audio.onerror = () => {
-        console.error("The audio file couldn't be loaded");
-      };
-      audio.play();
+      if (currentAudio) {
+        currentAudio.pause();
+        currentAudio.currentTime = 0;
+      }
+      currentAudio = new Audio(randomAudio);
+      currentAudio.play();
     }
   });
 
-box.addEventListener("mouseover", function() {
-  box.style.transform = "scale(1.2)";
-});
+  box.addEventListener("mouseover", function() {
+    box.style.transform = "scale(1.2)";
+  });
 
-box.addEventListener("mouseout", function() {
-  box.style.transform = "scale(1)";
-});
+  box.addEventListener("mouseout", function() {
+    box.style.transform = "scale(1)";
+  });
 
-dragElement(document.getElementById("box"));
+  dragElement(document.getElementById("box"));
 
-function dragElement(elmnt) {
-  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  if (document.getElementById(elmnt.id + "header")) {
-    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-  } else {
-    elmnt.onmousedown = dragMouseDown;
+  function dragElement(elmnt) {
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    if (document.getElementById(elmnt.id + "header")) {
+      document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+    } else {
+      elmnt.onmousedown = dragMouseDown;
+    }
+
+    function dragMouseDown(e) {
+      e = e || window.event;
+      e.preventDefault();
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      document.onmouseup = closeDragElement;
+      document.onmousemove = elementDrag;
+    }
+
+    function elementDrag(e) {
+      e = e || window.event;
+      e.preventDefault();
+      pos1 = pos3 - e.clientX;
+      pos2 = pos4 - e.clientY;
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+      elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    }
+
+    function closeDragElement() {
+      document.onmouseup = null;
+      document.onmousemove = null;
+    }
   }
-
-  function dragMouseDown(e) {
-    e = e || window.event;
-    e.preventDefault();
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
-    document.onmousemove = elementDrag;
-  }
-
-  function elementDrag(e) {
-    e = e || window.event;
-    e.preventDefault();
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-  }
-
-  function closeDragElement() {
-    document.onmouseup = null;
-    document.onmousemove = null;
-  }
-}
 });
