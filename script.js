dd6fb9cf7fa0b2def8e56477e9a9aaec640423b3
dick-fuck-face <124760072+dick-fuck-face@users.noreply.github.com>
2023-02-10 09:26:19 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 document.addEventListener("DOMContentLoaded", function() {
   const box = document.querySelector("#box");
-  const box2 = document.querySelector("#box2");
-  const emoji = document.querySelector("#emoji");
   const audioFolder = "https://api.github.com/repos/dick-fuck-face/big-dick-testing/contents/audio";
   let audioFiles = [];
   let currentAudio;
+
+  $(document).ready(function() {
+    $("#box").draggable({
+      axis: false,
+      helper: 'original'
+    });
+
+    $("#box2").draggable({
+      axis: false,
+      helper: 'original'
+    });
+
+  });
+
+  $("#emoji").click(function() {
+    $("#emoji").fadeOut();
+  });
 
   fetch(audioFolder)
     .then(res => res.json())
@@ -31,60 +46,4 @@ document.addEventListener("DOMContentLoaded", function() {
   box.addEventListener("mouseout", function() {
     box.style.transform = "scale(1)";
   });
-
-  dragElement(document.getElementById("box"));
-  
-  box2.addEventListener("mouseover", function() {
-  box2.style.transform = "scale(1.2)";
-});
-
-  box2.addEventListener("mouseout", function() {
-  box2.style.transform = "scale(1)";
-});
-
-  dragElement(document.getElementById("box2"));
-  
-  emoji.addEventListener("mouseover", function() {
-  emoji.style.transform = "scale(1.2)";
-});
-
-  emoji.addEventListener("mouseout", function() {
-  emoji.style.transform = "scale(1)";
-});
-
-  dragElement(document.getElementById("emoji"));
-
-  function dragElement(elmnt) {
-    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-    if (document.getElementById(elmnt.id + "header")) {
-      document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-    } else {
-      elmnt.onmousedown = dragMouseDown;
-    }
-
-    function dragMouseDown(e) {
-      e = e || window.event;
-      e.preventDefault();
-      pos3 = e.clientX;
-      pos4 = e.clientY;
-      document.onmouseup = closeDragElement;
-      document.onmousemove = elementDrag;
-    }
-
-    function elementDrag(e) {
-      e = e || window.event;
-      e.preventDefault();
-      pos1 = pos3 - e.clientX;
-      pos2 = pos4 - e.clientY;
-      pos3 = e.clientX;
-      pos4 = e.clientY;
-      elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-      elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-    }
-
-    function closeDragElement() {
-      document.onmouseup = null;
-      document.onmousemove = null;
-    }
-  }
 });
